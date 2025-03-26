395d9ccccfb08626a15346df79fb429cb9952d94
Eric McAfee <eric.c.mcafee@gmail.com>
2025-03-26 00:28:20 +0000

    Updated returned string in index page to test deployment through github.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+#Created by Eric McAfee
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Eric McAfee in 3308!'
