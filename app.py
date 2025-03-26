3ab95080ebacdd204f4d1e52364d024b0b492888
Eric McAfee <eric.c.mcafee@gmail.com>
2025-03-26 00:44:25 +0000

    Added the db_test route to connect to the render Postgres instance.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 #Created by Eric McAfee
 
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Eric McAfee in 3308!'
+
+@app.route('/db_test')
+def db_test():
+    conn = conn = psycopg2.connect("postgresql://render_tutorial_db_user:cQINwvX4yWTeP2NinFsZ2NIEA536CX6f@dpg-cvhkpkhc1ekc738cjee0-a/render_tutorial_db")
+    conn.close()
+    return "Database Connection Successful"
