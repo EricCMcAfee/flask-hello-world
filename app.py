a86e8299e0e9b5dee68d16d4d34db9db067ad764
Eric McAfee <eric.c.mcafee@gmail.com>
2025-03-26 00:52:12 +0000

    Created the db_create route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #Created by Eric McAfee
+#March 25th, 2025
 
 import psycopg2
 from flask import Flask
@@ -8,8 +9,25 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from Eric McAfee in 3308!'
 
-@app.route('/db_test')
+@app.route('/db_test') #establishes connection to our PostgreSQL db and returns a string to the browser to confirm.
 def db_test():
     conn = conn = psycopg2.connect("postgresql://render_tutorial_db_user:cQINwvX4yWTeP2NinFsZ2NIEA536CX6f@dpg-cvhkpkhc1ekc738cjee0-a/render_tutorial_db")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create') #Creates a table in the PostgrSQL db and returns a string to the browser to confirm.
+def db_create():
+    conn = conn = psycopg2.connect("postgresql://render_tutorial_db_user:cQINwvX4yWTeP2NinFsZ2NIEA536CX6f@dpg-cvhkpkhc1ekc738cjee0-a/render_tutorial_db")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
